740a1f0d7b6e5d11ecd0f236fbd5e18bb49edbc9
Xiaodong Ming <xiaodong.ming@outlook.com>
2019-12-04 21:58:41 +0000

    renew function to download nimrod radar rainfall files

--- example_DownloadNIMROD.py
+++ example_DownloadNIMROD.py
@@ -12,3 +12,20 @@ import nimrodProcessing as NP
 
 # your username and password on CEDA website
 NP.downloadNimrodTar(dateStr='20190131')
+tarList = glob.glob('*.tar')
+tarList.sort()
+
+#%% read DEM data to generate rainfall mask and rainfall source
+demFileName = 'eden_20m.asc'
+demArray,demHead,demExtent = AP.arcgridread(demFileName)
+
+# the start date and time of model simulation
+initalDatetime = datetime.datetime(2019,1,31,0,0)
+
+# rainMask is an array with the same size of DEM array
+# rainSource is an rainfall rate array with time series (1st col in seconds) 
+rainMask,rainSource=NF.getRainMaskSource(tarList,demHead,datetimeStart=initalDatetime)
+
+# write rainfall mask and rainfall source to files
+AP.arcgridwrite('rain_mask20m.asc',Z= rainMask,head=demHead)
+np.savetxt('rain_Source.txt',rainSource,fmt='%g')
