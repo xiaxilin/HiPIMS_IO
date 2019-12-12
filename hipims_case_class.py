9eaa014c88e9fffc59f4d4618aca2719f0a2cf24
Xiaodong Ming <xiaodong.ming@outlook.com>
2019-12-12 10:30:24 +0000

    Minor improvements

--- hipims_case_class.py
+++ hipims_case_class.py
@@ -1553,13 +1553,13 @@ def load_object(file_name):
     """ Read a pickle file as an InputHipims object
     """
     #read an InputHipims object file
-    print('loading '+file_name+'...')
     try:
         with gzip.open(file_name, 'rb') as input_file:
             obj = pickle.load(input_file)
     except:
         with open(file_name, 'rb') as input_file:
             obj = pickle.load(input_file)
+    print(file_name+' loaded')
     return obj
 
 def save_object(obj, file_name, compression=True):
